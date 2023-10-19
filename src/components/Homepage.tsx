1de40b9fc09fff950aefb54606d6ede760a9163b
linushahs <suniltraveler2004@gmail.com>
2023-10-19 17:26:20 +0545

    Made homepage fully responsive

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -5,19 +5,19 @@ function Homepage() {
   return (
     <section className="container relative py-8">
       <div className="flex items-center relative">
-        <h1 className="w-full">Digital Dilemmas,</h1>
-        <hr className="absolute top-1/2 right-0 w-[400px] border-tertiary" />
+        <h1 className="w-[90%] sm:w-[80%] 3xl:w-[85%]">Digital Dilemmas,</h1>
+        <hr className="absolute top-1/2 right-0 w-[15%] sm:w-[20%] lg:w-[25%] 3xl:w-[15%] border-tertiary" />
       </div>
 
       {/* headline, subtext -------------  */}
-      <div className="flex gap-8 items-start">
+      <div className="flex flex-col lg:flex-row gap-8 items-start">
         <h1>
           Problemos <br />
           <span className="text-primary">Solved</span>
         </h1>
 
-        <div className="w-1/3">
-          <p className="text-tertiary my-8 xl:text-lg 3xl:text-xl">
+        <div className="w-full sm:w-2/3 ml-auto lg:m-0 lg:w-1/3">
+          <p className="text-tertiary mb-8 lg:my-8 xl:text-lg 3xl:text-xl">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est fugit
             dolores, magni, itaque quis dicta asperiores omnis similique
             excepturi voluptate ullam nam fuga soluta eveniet ducimus beatae
@@ -27,7 +27,7 @@ function Homepage() {
           <span className="flex items-center gap-4">
             <button className="primary-btn">
               Get Started now
-              <ArrowUpRightIcon className="border border-background p-1 rounded-full w-8 h-8 " />
+              <ArrowUpRightIcon className="border border-background p-1 rounded-full w-6 h-6 sm:w-8 sm:h-8 " />
             </button>
             <hr className="w-full border-tertiary right-8" />
           </span>
@@ -35,7 +35,7 @@ function Homepage() {
       </div>
 
       {/* location, launched date ---------  */}
-      <div className="mt-6 mb-4 flex justify-between text-secondary text-lg">
+      <div className="mt-12 lg:mt-8 mb-4 flex justify-between text-secondary lg:text-lg">
         <h4>Kathmandu, Nepal</h4>
         <h4>2022 Nexus Marketers</h4>
       </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,32 @@
+"use client";
+
 import { ArrowUpRightIcon } from "@heroicons/react/24/outline";
 import Logo from "./assets/Logo";
+import { ArrowRightIcon, Bars3Icon } from "@heroicons/react/20/solid";
+import { useState } from "react";
+import { twMerge } from "tailwind-merge";
+
+type Menu = {
+  id: number;
+  title: string;
+  url: string;
+};
+
+export const menus: Menu[] = [
+  "home",
+  "about",
+  "work",
+  "team",
+  "project",
+  "blogs",
+  "contact",
+].map((menu, idx) => {
+  return { id: idx + 1, title: menu, url: "#" + menu };
+});
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <nav id="home" className="relative container  z-40  text-secondary ">
       <div className="border-b border-secondary flex items-center justify-between pt-3 pb-4">
@@ -32,11 +57,59 @@ function Navbar() {
           </li>
         </ul>
 
-        <div className="flex items-center capitalize gap-2">
+        <div className="hidden sm:flex items-center capitalize gap-2">
           <hr className="w-14 border-tertiary" />
           <h4>nexusmarketers@gmail.com</h4>
           <ArrowUpRightIcon className="border border-secondary p-2 rounded-full w-8 h-8 " />
         </div>
+
+        {/* bar icon ---------  */}
+        <button
+          onClick={() => setIsMenuOpen(true)}
+          className="lg:hidden cursor-pointer"
+        >
+          <Bars3Icon className="w-8 h-8 text-white" />
+        </button>
+      </div>
+
+      {/* backdrop : black ----------- */}
+      {isMenuOpen && (
+        <div className="fixed top-0 left-0 w-full h-screen z-20 bg-black/30"></div>
+      )}
+
+      {/* list of menus (in small devices: tab & mobile) ---->  */}
+      <div
+        className={twMerge(
+          "nav-menu top-0 right-0 fixed h-full w-[92%] sm:w-[70%] bg-background flex flex-col justify-between z-30",
+          isMenuOpen && "active"
+        )}
+      >
+        <header className="px-6 pt-6 flex w-full justify-between">
+          <Logo className="w-10 h-10 " />
+
+          <button
+            onClick={() => setIsMenuOpen(false)}
+            className="lg:hidden text-lg cursor-pointer font-bold"
+          >
+            Close
+          </button>
+        </header>
+
+        <ul className="px-6 flex-1 flex flex-col justify-center">
+          {menus.map((menu) => (
+            <li
+              key={menu.id}
+              className="my-2.5 py-3 text-3xl border-b border-tertiary font-medium  cursor-pointer hover:font-bold"
+            >
+              <a
+                className="flex justify-between items-center text-white capitalize"
+                href={menu.url}
+              >
+                {menu.title}
+              </a>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   );
