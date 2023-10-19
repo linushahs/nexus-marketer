b3b1b9e306cf88edb896e9a02b374f7829f61df9
linushahs <suniltraveler2004@gmail.com>
2023-10-19 17:53:12 +0545

    Added framer-motion with homepage animation

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,54 +1,111 @@
+"use client";
+
 import { ArrowUpRightIcon } from "@heroicons/react/20/solid";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 function Homepage() {
   return (
     <section className="container relative py-8">
       <div className="flex items-center relative">
-        <h1 className="w-[90%] sm:w-[80%] 3xl:w-[85%]">Digital Dilemmas,</h1>
-        <hr className="absolute top-1/2 right-0 w-[15%] sm:w-[20%] lg:w-[25%] 3xl:w-[15%] border-tertiary" />
+        <motion.h1
+          initial={{ x: -100, opacity: 0 }}
+          animate={{
+            x: 0,
+            opacity: 100,
+            transition: { delay: 1, duration: 1 },
+          }}
+          className="w-[90%] sm:w-[80%] 3xl:w-[85%]"
+        >
+          Digital Dilemmas,
+        </motion.h1>
+        <motion.hr
+          initial={{ y: "-100%", opacity: 0 }}
+          animate={{ y: 0, opacity: 100, transition: { delay: 1 } }}
+          className="absolute top-1/2 right-0 w-[15%] sm:w-[20%] lg:w-[25%] 3xl:w-[15%] border-tertiary"
+        />
       </div>
 
       {/* headline, subtext -------------  */}
       <div className="flex flex-col lg:flex-row gap-8 items-start">
-        <h1>
+        <motion.h1
+          initial={{ x: -100, opacity: 0 }}
+          animate={{
+            x: 0,
+            opacity: 100,
+            transition: { delay: 1, duration: 1 },
+          }}
+        >
           Problemos <br />
           <span className="text-primary">Solved</span>
-        </h1>
+        </motion.h1>
 
         <div className="w-full sm:w-2/3 ml-auto lg:m-0 lg:w-1/3">
-          <p className="text-tertiary mb-8 lg:my-8 xl:text-lg 3xl:text-xl">
+          <motion.p
+            initial={{ y: -50, opacity: 0 }}
+            animate={{
+              y: 0,
+              opacity: 100,
+              transition: { delay: 1.2, duration: 1 },
+            }}
+            className="text-tertiary mb-8 lg:my-8 xl:text-lg 3xl:text-xl"
+          >
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est fugit
             dolores, magni, itaque quis dicta asperiores omnis similique
             excepturi voluptate ullam nam fuga soluta eveniet ducimus beatae
             debitis. Ea, nostrum.
-          </p>
+          </motion.p>
 
-          <span className="flex items-center gap-4">
+          <motion.span
+            initial={{ x: -50, opacity: 0 }}
+            animate={{
+              x: 0,
+              opacity: 100,
+              transition: { delay: 1.5, duration: 1 },
+            }}
+            className="flex items-center gap-4"
+          >
             <button className="primary-btn">
               Get Started now
               <ArrowUpRightIcon className="border border-background p-1 rounded-full w-6 h-6 sm:w-8 sm:h-8 " />
             </button>
             <hr className="w-full border-tertiary right-8" />
-          </span>
+          </motion.span>
         </div>
       </div>
 
       {/* location, launched date ---------  */}
-      <div className="mt-12 lg:mt-8 mb-4 flex justify-between text-secondary lg:text-lg">
+      <motion.div
+        initial={{ y: -50, opacity: 0 }}
+        animate={{
+          y: 0,
+          opacity: 100,
+          transition: { delay: 1.7, duration: 0.6 },
+        }}
+        className="mt-12 lg:mt-8 mb-4 flex justify-between text-secondary lg:text-lg"
+      >
         <h4>Kathmandu, Nepal</h4>
         <h4>2022 Nexus Marketers</h4>
-      </div>
+      </motion.div>
 
       {/* thumbnail ----------  */}
-      <Image
-        src="/assets/landing-thumbnail.png"
-        alt="thumbnail"
-        width={1000}
-        height={200}
-        priority={true}
-        className="w-full"
-      />
+      <motion.div
+        initial={{ x: -100, opacity: 0 }}
+        animate={{
+          x: 0,
+          opacity: 100,
+          transition: { delay: 2.2, duration: 0.6 },
+        }}
+      >
+        <Image
+          src="/assets/landing-thumbnail.png"
+          alt="thumbnail"
+          width={1000}
+          height={200}
+          priority={true}
+          className="w-full"
+        />
+      </motion.div>
     </section>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,7 @@ import Logo from "./assets/Logo";
 import { ArrowRightIcon, Bars3Icon } from "@heroicons/react/20/solid";
 import { useState } from "react";
 import { twMerge } from "tailwind-merge";
+import { motion } from "framer-motion";
 
 type Menu = {
   id: number;
@@ -29,12 +30,23 @@ function Navbar() {
 
   return (
     <nav id="home" className="relative container  z-40  text-secondary ">
-      <div className="border-b border-secondary flex items-center justify-between pt-3 pb-4">
-        <div>
+      <div className=" flex items-center justify-between pt-3 pb-4">
+        <motion.div
+          initial={{ y: "-100%", opacity: 0 }}
+          animate={{ y: 0, opacity: 100, transition: { duration: 0.5 } }}
+        >
           <Logo className="w-12 h-12" />
-        </div>
+        </motion.div>
         {/* right side  */}
-        <ul className="navbar-menu lg:flex items-center ml-32 gap-16 hidden font-[500] text-lg 3xl:text-xl">
+        <motion.ul
+          initial={{ y: "-100%", opacity: 0 }}
+          animate={{
+            y: 0,
+            opacity: 100,
+            transition: { delay: 0.2, duration: 0.8 },
+          }}
+          className="navbar-menu lg:flex items-center ml-32 gap-16 hidden font-[500] text-lg 3xl:text-xl"
+        >
           <li>
             <a href="#home" className="active">
               Home
@@ -55,13 +67,21 @@ function Navbar() {
           <li>
             <a href="#blogs">Blogs</a>
           </li>
-        </ul>
+        </motion.ul>
 
-        <div className="hidden sm:flex items-center capitalize gap-2">
+        <motion.div
+          initial={{ y: "-100%", opacity: 0 }}
+          animate={{
+            y: 0,
+            opacity: 100,
+            transition: { delay: 0.3, duration: 0.5 },
+          }}
+          className="hidden sm:flex items-center capitalize gap-2"
+        >
           <hr className="w-14 border-tertiary" />
           <h4>nexusmarketers@gmail.com</h4>
           <ArrowUpRightIcon className="border border-secondary p-2 rounded-full w-8 h-8 " />
-        </div>
+        </motion.div>
 
         {/* bar icon ---------  */}
         <button
@@ -71,6 +91,12 @@ function Navbar() {
           <Bars3Icon className="w-8 h-8 text-white" />
         </button>
       </div>
+
+      <motion.hr
+        initial={{ y: "-100%", opacity: 0 }}
+        animate={{ y: 0, opacity: 100, transition: { delay: 0.5 } }}
+        className="border-tertiary "
+      />
 
       {/* backdrop : black ----------- */}
       {isMenuOpen && (
