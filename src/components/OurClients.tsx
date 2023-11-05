0f60ff6394d6e031aaeacec53197a077bb2df64c
linushahs <suniltraveler2004@gmail.com>
2023-11-05 14:50:02 +0545

    Made our clients section responsive

--- src/components/OurClients.tsx
+++ src/components/OurClients.tsx
@@ -1,24 +1,41 @@
 import * as Icon from "@/components/assets/icons";
 
+const iconStyle = "w-[110px] sm:w-[140px] xl:w-[200px] text-white";
+
 function OurClients() {
   return (
     <section id="clients" className="container py-10">
       <h1 className="text-center">our clients</h1>
 
-      <main className="mt-12 flex flex-col items-center gap-16">
-        <div className="flex gap-[120px]">
-          <Icon.Adobe className=" text-white" />
-          <Icon.GitLab className="text-white" />
-          <Icon.ProductBoard className="text-white" />
-          <Icon.Adobe className="text-white" />
+      <main className="mt-10 sm:mt-16 flex flex-col items-center gap-y-10 lg:gap-y-16">
+        <div className="flex justify-center flex-wrap gap-x-[40px] sm:gap-x-[120px] gap-y-8">
+          <Icon.Adobe className={iconStyle} />
+          <Icon.GitLab className={iconStyle} />
+          <Icon.Adobe className={iconStyle} />
+          <Icon.ProductBoard
+            className={iconStyle + " !w-[180px] sm:!w-[220px] lg:!w-[260px]"}
+          />
         </div>
-        <div className="flex gap-[90px]">
-          <Icon.AirWallex className="text-white" />
-          <Icon.Adobe className="text-white" />
-          <Icon.GitLab className="text-white" />
+        <div className="flex flex-wrap justify-center gap-x-[40px] gap-y-8  sm:gap-x-[90px]">
+          <Icon.AirWallex
+            className={iconStyle + " !w-[160px] sm:!w-[180px] lg:!w-[210px]"}
+          />
+          <Icon.Adobe className={iconStyle} />
+          <Icon.GitLab className={iconStyle} />
+
+          <Icon.AirWallex
+            className={
+              iconStyle + " md:hidden !w-[160px] sm:!w-[180px] lg:!w-[210px]"
+            }
+          />
         </div>
 
-        <Icon.AirWallex className="text-white" />
+        <Icon.AirWallex
+          className={
+            iconStyle +
+            " hidden md:block !w-[160px] sm:!w-[180px] lg:!w-[210px]"
+          }
+        />
       </main>
     </section>
   );
