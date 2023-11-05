a7b790f869a6e75e736e06f12cf5de5d808869f5
linushahs <suniltraveler2004@gmail.com>
2023-11-05 14:18:53 +0545

    Made work process responsive

--- src/components/WorkProcess.tsx
+++ src/components/WorkProcess.tsx
@@ -6,21 +6,21 @@ function WorkProcess() {
     <section id="work" className="container py-10  ">
       <h1>How do we work?</h1>
 
-      <main className="flex gap-6  mt-16">
+      <main className="flex flex-col gap-y-12 lg:flex-row lg:gap-6  mt-16">
         {workProcess.map((process) => (
           <div key={process.step} className="text-secondary">
-            <span className="rounded-full border border-secondary px-4 py-1 text-lg font-bold">
+            <span className="rounded-full border border-secondary px-4 py-1 text-lg sm:text-xl lg:text-lg font-bold">
               Step {process.step}
             </span>
-            <h4 className="mt-3 text-lg sm:text-xl lg:text-2xl">
-              {process.title}
-            </h4>
+            <h4 className="mt-3  text-2xl ">{process.title}</h4>
 
             <div
-              className="mt-4 bg-no-repeat h-[400px] rounded-lg p-8 flex items-end"
+              className="mt-4 bg-no-repeat w-full h-[300px] sm:h-[350px] bg-cover lg:h-[400px] rounded-lg p-8 flex items-end"
               style={{ backgroundImage: `url(${process.imgSrc})` }}
             >
-              <p className="text-lg ">{process.description}</p>
+              <p className="text-xl w-full sm:text-2xl sm:w-3/4 lg:w-full lg:text-lg ">
+                {process.description}
+              </p>
             </div>
           </div>
         ))}
