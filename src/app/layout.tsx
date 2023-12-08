876cba9d2dc8d58b943146ab768f1e20c7adc022
linushahs <suniltraveler2004@gmail.com>
2023-12-08 16:38:24 +0545

    Fixed fb chat plugin issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 // Import Swiper styles
+import Script from "next/script";
 import "swiper/css";
 import "swiper/css/free-mode";
-import FacebookChat from "./FacebookChat";
 
 export const metadata: Metadata = {
   title: "Nexus Marketer",
@@ -20,7 +20,31 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
-        <FacebookChat />
+
+        <div id="fb-root"></div>
+        <div id="fb-customer-chat" className="fb-customerchat"></div>
+        <Script id="fb-chat" strategy="lazyOnload">
+          {`
+          var chatbox = document.getElementById('fb-customer-chat');  
+          chatbox.setAttribute("page\_id", "YOUR\_PAGE\_ID");  
+          chatbox.setAttribute("attribution", "biz\_inbox");  
+            
+          window.fbAsyncInit = function() {  
+          FB.init({  
+          xfbml            : true,  
+          version          : 'v12.0'  
+          });  
+          };  
+            
+          (function(d, s, id) {  
+          var js, fjs = d.getElementsByTagName(s)\[0\];  
+          if (d.getElementById(id)) return;  
+          js = d.createElement(s); js.id = id;  
+          js.src = 'https://connect.facebook.net/en\_US/sdk/xfbml.customerchat.js';  
+          fjs.parentNode.insertBefore(js, fjs);  
+          }(document, 'script', 'facebook-jssdk'));  
+      `}
+        </Script>
       </body>
     </html>
   );
