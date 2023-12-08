bda2cd5dcf18b24124ebe99c2b59a12bae3e2b8c
linushahs <suniltraveler2004@gmail.com>
2023-12-08 17:07:30 +0545

    Minor script changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,24 +25,25 @@ export default function RootLayout({
         <div id="fb-customer-chat" className="fb-customerchat"></div>
         <Script id="fb-chat" strategy="lazyOnload">
           {`
-          var chatbox = document.getElementById('fb-customer-chat');  
-          chatbox.setAttribute("page\_id", "105097512671054");  
-          chatbox.setAttribute("attribution", "biz\_inbox");  
-            
-          window.fbAsyncInit = function() {  
-          FB.init({  
-          xfbml            : true,  
-          version          : 'v18.0'  
-          });  
-          };  
-            
-          (function(d, s, id) {  
-          var js, fjs = d.getElementsByTagName(s)\[0\];  
-          if (d.getElementById(id)) return;  
-          js = d.createElement(s); js.id = id;  
-          js.src = 'https://connect.facebook.net/en\_US/sdk/xfbml.customerchat.js';  
-          fjs.parentNode.insertBefore(js, fjs);  
-          }(document, 'script', 'facebook-jssdk'));  
+        
+          var chatbox = document.getElementById('fb-customer-chat');
+          chatbox.setAttribute("page_id", "105097512671054");
+          chatbox.setAttribute("attribution", "biz_inbox");
+  
+          window.fbAsyncInit = function() {
+            FB.init({
+              xfbml            : true,
+              version          : 'v18.0'
+            });
+          };
+
+          (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+            fjs.parentNode.insertBefore(js, fjs);
+          }(document, 'script', 'facebook-jssdk')); 
       `}
         </Script>
       </body>
